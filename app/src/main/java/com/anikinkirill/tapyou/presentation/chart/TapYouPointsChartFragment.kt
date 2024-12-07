3f7060d3b635e97f3497f609d9d60d6c1f2a8554
anikinkirill <anikinkirill@yandex-team.ru>
2024-12-07 20:22:58 +0300

    Save image to local storage feature

--- app/src/main/java/com/anikinkirill/tapyou/presentation/chart/TapYouPointsChartFragment.kt
+++ app/src/main/java/com/anikinkirill/tapyou/presentation/chart/TapYouPointsChartFragment.kt
@@ -1,5 +1,8 @@
 package com.anikinkirill.tapyou.presentation.chart
 
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
@@ -7,6 +10,10 @@ import android.widget.LinearLayout.LayoutParams
 import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -31,7 +38,8 @@ class TapYouPointsChartFragment : Fragment(R.layout.fragment_tap_you_points_char
         super.onViewCreated(view, savedInstanceState)
         initTopBar()
         loadPoints()
-        observe()
+        observeState()
+        observeActions()
     }
 
     private fun initTopBar() {
@@ -39,15 +47,54 @@ class TapYouPointsChartFragment : Fragment(R.layout.fragment_tap_you_points_char
         binding.topBar.back.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
+        binding.topBar.rightActionButton.setImageDrawable(
+            ResourcesCompat.getDrawable(resources, R.drawable.ic_copy, null)
+        )
+
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                saveImageToLocalStorage()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    requireContext().getString(
+                        R.string.no_permission_to_save_image,
+                    ),
+                    Toast.LENGTH_LONG,
+                ).show()
+            }
+        }
+
+        binding.topBar.rightActionButton.setOnClickListener {
+            checkForPermissions(requestPermissionLauncher)
+        }
     }
 
-    private fun observe() {
+    private fun observeState() {
         lifecycleScope.launch {
             viewModel.collect { state ->
                 when {
                     state.errorState != null -> showErrorState(text = state.errorState.errorText)
                     state.isLoading -> showLoadingState()
                     state.points.isNotEmpty() -> showPointsChart(points = state.points)
+                }
+            }
+        }
+    }
+
+    private fun observeActions() {
+        lifecycleScope.launch {
+            viewModel.default.effectFlow.collect { action ->
+                when (action) {
+                    is TapYouPointsChartViewModel.Action.ShowToast -> {
+                        Toast.makeText(
+                            requireContext(),
+                            action.message.getString(requireContext()),
+                            Toast.LENGTH_LONG,
+                        ).show()
+                    }
                 }
             }
         }
@@ -74,12 +121,20 @@ class TapYouPointsChartFragment : Fragment(R.layout.fragment_tap_you_points_char
                 // x and y entries in row
                 val xEntry = TextView(context).apply {
                     text = point.x.toString()
-                    layoutParams = TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f)
+                    layoutParams = TableRow.LayoutParams(
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT,
+                        1f
+                    )
                     gravity = Gravity.CENTER
                 }
                 val yEntry = TextView(context).apply {
                     text = point.y.toString()
-                    layoutParams = TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f)
+                    layoutParams = TableRow.LayoutParams(
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT,
+                        1f
+                    )
                     gravity = Gravity.CENTER
                 }
                 row.addView(xEntry)
@@ -109,6 +164,29 @@ class TapYouPointsChartFragment : Fragment(R.layout.fragment_tap_you_points_char
 
     private fun getAmountFromArgs(): Int? {
         return arguments?.getInt(ARGS_KEY)
+    }
+
+    private fun checkForPermissions(permissionsLauncher: ActivityResultLauncher<String>) {
+        val isGranted = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+        if (isGranted) {
+            saveImageToLocalStorage()
+        } else {
+            permissionsLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun saveImageToLocalStorage() {
+        val bitmap = Bitmap.createBitmap(
+            binding.root.width,
+            binding.root.height,
+            Bitmap.Config.ARGB_8888,
+        )
+        val canvas = Canvas(bitmap)
+        binding.root.draw(canvas)
+        viewModel.saveImageToLocalStorage(bitmap, requireContext())
     }
 
     companion object {
--- app/src/main/java/com/anikinkirill/tapyou/presentation/chart/TapYouPointsChartViewModel.kt
+++ app/src/main/java/com/anikinkirill/tapyou/presentation/chart/TapYouPointsChartViewModel.kt
@@ -1,6 +1,13 @@
 package com.anikinkirill.tapyou.presentation.chart
 
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.lifecycle.viewModelScope
+import com.anikinkirill.tapyou.R
 import com.anikinkirill.tapyou.domain.Resource.Companion.onError
 import com.anikinkirill.tapyou.domain.Resource.Companion.onSuccess
 import com.anikinkirill.tapyou.domain.Text
@@ -9,7 +16,12 @@ import com.anikinkirill.tapyou.domain.usecase.GetTapYouPointsUseCase
 import com.anikinkirill.tapyou.presentation.chart.TapYouPointsChartViewModel.Action
 import com.anikinkirill.tapyou.presentation.chart.TapYouPointsChartViewModel.State
 import com.anikinkirill.tapyou.presentation.utils.BaseViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class TapYouPointsChartViewModel(
     private val useCase: GetTapYouPointsUseCase,
@@ -42,6 +54,46 @@ class TapYouPointsChartViewModel(
         }
     }
 
+    fun saveImageToLocalStorage(bitmap: Bitmap, context: Context) {
+        var outputStream: OutputStream?
+        val filename = "tap_you_image_${System.currentTimeMillis()}.png"
+        viewModelScope.launch {
+            withContext(Dispatchers.Default) {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        val contentValues = ContentValues().apply {
+                            put(
+                                MediaStore.MediaColumns.DISPLAY_NAME,
+                                filename
+                            )
+                            put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                        }
+                        val uri = context.contentResolver.insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            contentValues
+                        )
+                        outputStream = uri?.let { context.contentResolver.openOutputStream(it) }
+                    } else {
+                        val imagesDir = Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_PICTURES
+                        )
+                        val image = File(imagesDir, filename)
+                        outputStream = FileOutputStream(image)
+                    }
+                } catch (exception: Exception) {
+                    outputStream = null
+                }
+                outputStream?.let { os ->
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+                    postSideEffect(Action.ShowToast(Text.res(R.string.image_is_successfully_saved)))
+                } ?: kotlin.run {
+                    postSideEffect(Action.ShowToast(Text.res(R.string.image_is_not_saved)))
+                }
+            }
+        }
+    }
+
     data class State(
         val points: List<TapYouPoints> = emptyList(),
         val isLoading: Boolean = false,
@@ -52,5 +104,7 @@ class TapYouPointsChartViewModel(
         val errorText: Text,
     )
 
-    sealed class Action
+    sealed class Action {
+        data class ShowToast(val message: Text) : Action()
+    }
 }
