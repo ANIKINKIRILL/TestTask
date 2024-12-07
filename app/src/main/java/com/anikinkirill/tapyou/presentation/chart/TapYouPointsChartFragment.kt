dba2776bc7c358ffe62b2e52b9bc951be5044d44
anikinkirill <anikinkirill@yandex-team.ru>
2024-12-07 19:23:50 +0300

    Redesign fragment_tap_you_config.xml

--- app/src/main/java/com/anikinkirill/tapyou/presentation/chart/TapYouPointsChartFragment.kt
+++ app/src/main/java/com/anikinkirill/tapyou/presentation/chart/TapYouPointsChartFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout.LayoutParams
-import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
